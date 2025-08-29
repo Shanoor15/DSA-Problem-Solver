5fd8d894f82c00fbaede7349431c09b8185bde42
SHANOOR <SHANOOR@LAPTOP-Q2AOCI85>
2025-08-29 12:25:27 +0530

    Change2

--- RearrangeArrayElements/src/RearrangeArrayElements.java
+++ RearrangeArrayElements/src/RearrangeArrayElements.java
@@ -7,10 +7,10 @@ public class RearrangeArrayElements {
 		System.out.println(Arrays.toString(result));
 	}
 	//1.Brute Level
-	public static int[] arrangArray(int[] arr,int n){
+	/*public static int[] arrangArray(int[] arr,int n){
 	    //ArrayList<Integer> list=new ArrayList<>();
 	    List<Integer> ne = new ArrayList<>();
-        List<Integer> po = new ArrayList<>();
+	    List<Integer> po = new ArrayList<>();
 	    for(int i=0;i<n;i++){
 	        if(arr[i]>=0){
 	            po.add(arr[i]);
@@ -19,17 +19,37 @@ public class RearrangeArrayElements {
 	            ne.add(arr[i]);
 	        }
 	    }
-	    /*int p=0,ng=0,i=0;
-	    while(p<po.size() && ng<ne.size()){
-	        arr[i++]=po.get(p++);
-	        arr[i++]=ne.get(ng++);
-	    }*/
+	//	    int p=0,ng=0,i=0;
+	//	    while(p<po.size() && ng<ne.size()){
+	//	        arr[i++]=po.get(p++);
+	//	        arr[i++]=ne.get(ng++);
+	//	    }
 	    int p=0;
 	    for(int i=0;i<n/2;i++){
 	        arr[2*i]=po.get(i);
 	        arr[2*i+1]=ne.get(i);
 	    }
 	    return arr;
+	}*/
+	
+	//2.Optimal Level
+	public static int[] arrangArray(int[] arr,int n){
+		int[] ans=new int[n];
+		int po=0,neg=1;
+		for(int i=0;i<n;i++) {
+			if(arr[i]>=0) {
+				if(po<n) {
+					ans[po]=arr[i];
+					po+=2;
+				}
+			}else {
+				if(neg<n) {
+					ans[neg]=arr[i];
+					neg+=2;
+				}
+			}
+		}
+		return ans;
 	}
 
 }
