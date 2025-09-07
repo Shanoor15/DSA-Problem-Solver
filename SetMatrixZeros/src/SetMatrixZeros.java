1d9344d29380c74285cd6b8566d4284153bd41df
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-07 23:47:33 +0530

    Change3

--- SetMatrixZeros/src/SetMatrixZeros.java
+++ SetMatrixZeros/src/SetMatrixZeros.java
@@ -16,7 +16,8 @@ public class SetMatrixZeros {
         }
 	}
 	
-	public static void setMatrixZeros(int[][] matrix,int n,int m){
+	//1.Brute Level
+	/*public static void setMatrixZeros(int[][] matrix,int n,int m){
 	    // Step 1: Mark rows and columns
 	    for(int i=0;i<n;i++){
 	        for(int j=0;j<m;j++){
@@ -48,6 +49,61 @@ public class SetMatrixZeros {
 	               matrix[i][j]=-1;
 	           }
 	       }
-	   }
-
+	   }*/
+	   
+	//2.Better Level
+	/*public static void setMatrixZeros(int[][] matrix,int n,int m){
+	    int[] row=new int[n];
+	    int[] col=new int[m];
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(matrix[i][j]==0){
+	                row[i]=1;
+	                col[j]=1;
+	            }
+	        }
+	    }
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<n;j++){
+	            if(row[i]==1 || col[j]==1){
+	                matrix[i][j]=0;
+	            }
+	        }
+	    }
+	}*/
+	
+	//3.Optimal Level
+	public static void setMatrixZeros(int[][] matrix,int n,int m){
+	    int col=1;
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){
+	            if(matrix[i][j]==0){
+	                matrix[i][0]=0;
+	                if(j!=0)
+	                    matrix[0][j]=0;
+	                else
+	                    col=0;
+	            }
+	        }
+	    }
+	    for(int i=1;i<n;i++){
+	        for(int j=1;j<m;j++){
+	            if(matrix[i][j]!=0){
+	                //check for col & row
+	                if(matrix[0][j]==0 || matrix[i][0]==0){
+	                    matrix[i][j]=0;
+	                }
+	            }
+	        }
+	    }
+	    if(matrix[0][0]==0){
+	        for(int j=0;j<m;j++)
+	            matrix[0][j]=0;
+	    }
+	    if(col==0){
+	        for(int i=0;i<n;i++){
+	            matrix[i][0]=0;
+	        }
+	    }
+	}
 }
