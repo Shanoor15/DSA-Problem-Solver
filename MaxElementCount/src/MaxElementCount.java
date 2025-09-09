787958f463acca26f1142561d3239d0650ce943d
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-09 13:52:10 +0530

    Change2

--- MaxElementCount/src/MaxElementCount.java
+++ MaxElementCount/src/MaxElementCount.java
@@ -16,12 +16,21 @@ public class MaxElementCount {
 	    return cnt;
 	}*/
 	
-	public static int maxElementCount(int[] arr,int number){
+	//2.Better Level
+	/*public static int maxElementCount(int[] arr,int number){
 	    int[] hash=new int[12];
 	    for(int i=0;i<arr.length;i++){
 	        hash[arr[i]]+=1;
 	    }
 	    return hash[number];
-	}
+	}*/
 
+	//3.Optimal Level
+	public static int maxElementCount(int[] arr,int number){
+		HashMap<Integer,Integer> map=new HashMap<>();
+		for(int i=0;i<arr.length;i++) {
+			map.put(arr[i],map.getOrDefault(arr[i], 0)+1);
+	}
+		return map.get(number);
+	}
 }
