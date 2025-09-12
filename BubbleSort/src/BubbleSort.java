e0ff3446244da434e8eb69285ce31ce33164c07c
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-12 10:32:01 +0530

    Change1

---
+++ BubbleSort/src/BubbleSort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] arr={4,1,5,2,3};
+		int n=arr.length;
+		int[] result=bubbleSort(arr,n);
+		System.out.println(Arrays.toString(result));
+	}
+	public static int[] bubbleSort(int[] arr,int n){
+	    for(int i=0;i<n-1;i++){
+	        for(int j=0;j<n-i-1;j++){
+	            if(arr[j]>arr[j+1]){
+	                int temp=arr[j];
+	                arr[j]=arr[j+1];
+	                arr[j+1]=temp;
+	            }
+	        }
+	    }
+	    return arr;
+	}
+
+}
