3c3f5736795e818c2f7f62ecbc40906a16b258b6
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-12 10:39:16 +0530

    Change2

--- BubbleSort/src/BubbleSort.java
+++ BubbleSort/src/BubbleSort.java
@@ -7,14 +7,18 @@ public class BubbleSort {
 		System.out.println(Arrays.toString(result));
 	}
 	public static int[] bubbleSort(int[] arr,int n){
+	    boolean isSwap=false;
 	    for(int i=0;i<n-1;i++){
 	        for(int j=0;j<n-i-1;j++){
 	            if(arr[j]>arr[j+1]){
 	                int temp=arr[j];
 	                arr[j]=arr[j+1];
 	                arr[j+1]=temp;
+	                isSwap=true;
 	            }
 	        }
+	        if(!isSwap)//array is already sorted
+	            return arr;
 	    }
 	    return arr;
 	}
