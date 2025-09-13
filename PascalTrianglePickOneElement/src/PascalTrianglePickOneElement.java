b9a0f26e5b9360cde199fc92c53fb828e63b9b09
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-09-13 13:13:44 +0530

    Change1

---
+++ PascalTrianglePickOneElement/src/PascalTrianglePickOneElement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class PascalTrianglePickOneElement {
+	public static void main(String[] args) {
+		int[][] arr = { 
+				{ 1 }, 
+				{ 1, 1 }, 
+				{ 1, 2, 1 }, 
+				{ 1, 3, 3, 1 }, 
+				{ 1, 4, 6, 4, 1 }, 
+				{ 1, 5, 10, 10, 5, 1 },
+				{ 1, 6, 15, 20, 15, 6, 1 } 
+				};
+		int r = 7, ele = 5;
+		int result = funNCR(arr, r - 1, ele - 1);
+		System.out.println(result);
+	}
+
+	public static int funNCR(int[][] arr, int r, int ele) {
+		int res = 1;
+		for (int i = 0; i < ele; i++) {
+			res = res * (r - i);
+			res = res / (i + 1);
+		}
+		return res;
+	}
+
+}
