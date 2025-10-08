38ad383e381725c3b227d7b00768c287e7bf9c4e
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-10-08 23:06:15 +0530

    Change3

--- MergOverlappingIntervel/src/MergOverLappingIntervel.java
+++ MergOverlappingIntervel/src/MergOverLappingIntervel.java
@@ -11,9 +11,9 @@ public class MergOverLappingIntervel {
             {15, 18},
             {16, 17}
         };
-        int[][] res = mergeIntervals(arr); // ✅ changed return type
-        for (int[] row : res) {
-            System.out.println(Arrays.toString(row));
+        List<List<Integer>> res = mergeIntervals(arr); // ✅ changed return type
+        for (List<Integer> row : res) {
+            System.out.println(row);
         }
     }
 
@@ -38,31 +38,46 @@ public class MergOverLappingIntervel {
 		}
 		return ans;*/
   
-    private static int[][] mergeIntervals(int[][] arr) {
-            int n = arr.length;
-            Arrays.sort(arr, (a, b) -> a[0] - b[0]); 
-            List<int[]> ans = new ArrayList<>();
-            
-            for (int i = 0; i < n; i++) {
-                int start = arr[i][0];
-                int end = arr[i][1];
-              
-                if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
-                    continue;
-                }
-                
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j][0] <= end) {
-                        end = Math.max(end, arr[j][1]);
-                    } else {
-                        break;
-                    }
-                }
-                ans.add(new int[]{start, end});
-            }
-            
-            return ans.toArray(new int[ans.size()][]);
-        }
-
-
+		/*private static int[][] mergeIntervals(int[][] arr) {
+		        int n = arr.length;
+		        Arrays.sort(arr, (a, b) -> a[0] - b[0]); 
+		        List<int[]> ans = new ArrayList<>();
+		        
+		        for (int i = 0; i < n; i++) {
+		            int start = arr[i][0];
+		            int end = arr[i][1];
+		          
+		            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1)[1]) {
+		                continue;
+		            }
+		            
+		            for (int j = i + 1; j < n; j++) {
+		                if (arr[j][0] <= end) {
+		                    end = Math.max(end, arr[j][1]);
+		                } else {
+		                    break;
+		                }
+		            }
+		            ans.add(new int[]{start, end});
+		        }
+		        
+		        return ans.toArray(new int[ans.size()][]);
+		    }*/
+    //2.
+    private static List<List<Integer>> mergeIntervals(int[][] arr) {
+    	Arrays.sort(arr,(a,b)->a[0]-b[0]);
+    	List<List<Integer>> ans=new ArrayList<>();
+    	int start=arr[0][0],end=arr[0][1];
+    	for(int i=1;i<arr.length;i++) {
+    		if(arr[i][0]<=end) {
+    			end=Math.max(end, arr[i][1]);
+    		}else{
+    			ans.add(Arrays.asList(start,end));
+    			start=arr[i][0];
+    			end=arr[i][1];
+    		}
+    	}
+    	ans.add(Arrays.asList(start,end));
+    	return ans;
+    }
 }
