1d3114922bb2d4c62303bc74aa82da68abaeb09b
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-10-14 21:37:59 +0530

    Change1

---
+++ Factorial/src/Factorial.java
@@ -0,0 +1,13 @@
+
+public class Factorial {
+	public static void main(String[] args) {
+	    int n=5;
+		int res = factorial(n);
+		System.out.println(res);
+	}
+    public static int factorial(int n) {
+        if(n==0) return 1;
+        return n*factorial(n-1);
+    }
+
+}
