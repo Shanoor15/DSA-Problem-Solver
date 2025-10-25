a8757819400a7f6abfe12c9b8c3b52b9d38579c0
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-10-25 23:33:13 +0530

    Change2

--- SearchInRotatedSortedArray-II/src/SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray-II/src/SearchInRotatedSortedArray.java
@@ -16,18 +16,32 @@ public class SearchInRotatedSortedArray {
 	}*/
 	
 	public static boolean search(int[] arr,int target){
-	    Arrays.sort(arr);
-	    int left=0,right=arr.length-1;
-	    while(left<=right){
-	        int mid=(left+right)/2;
-	        if(arr[mid]==target)
-	            return true;
-	        if(arr[mid]>target)
-	            right=mid-1;
-	        else
-	            left=mid+1;
-	    }
-	    return false;
+		 int low=0,high=arr.length-1;
+	        while(low<=high){
+	            int mid=(low+high)/2;
+	            if(arr[mid]==target){
+	                return true;
+	            }
+	            if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+	                low=low+1;
+	                high=high-1;
+	                continue;
+	            }
+	            if(arr[low]<=arr[mid]){
+	                if(arr[low]<=target && target<=arr[mid]){
+	                    high=mid-1;
+	                }else{
+	                    low=mid+1;
+	                }
+	            }else{
+	                if(arr[mid]<=target && target<=arr[high]){
+	                    low=mid+1;
+	                }else{
+	                    high=mid-1;
+	                }
+	            }
+	        }
+	        return false;
 	}
 
 }
