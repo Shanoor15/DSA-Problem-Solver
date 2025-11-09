592d362a96fc24cb78ca469782a74aef3c9c5bd8
Shanoor15 <shanoorsyed1707100@gmail.com>
2025-11-09 23:24:06 +0530

    Change1

---
+++ CheckStringPalindrom/src/CheckStringPalindrom.java
@@ -0,0 +1,34 @@
+
+public class CheckStringPalindrom {
+	public static void main(String[] args) {
+		String s="madam";
+		boolean res=palindrom(s);
+		System.out.println(res);
+	}
+	//1.Better Level
+	/*public static boolean palindrom(String s) {
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		//return s.toLowerCase().equals(rev.toLowerCase());
+		return s.equalsIgnoreCase(rev);
+	}*/
+	
+	//2.Optimal Level
+	public static boolean palindrom(String s) {
+		int l=0,r=s.length()-1;
+		while(l<r) {
+			/*if(s.charAt(l)!=(s.charAt(r))) {
+				return false;
+			}*/
+			if(Character.toLowerCase(s.charAt(l))!=Character.toLowerCase(s.charAt(r))) {
+				return false;
+			}
+			l++;
+			r--;
+		}
+		return true;
+	}
+
+}
